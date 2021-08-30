ef10055b4388943f810c9898408f767955cf4912
VerveIsBad <89467097+VerveIsBad@users.noreply.github.com>
2021-08-30 19:30:58 -0400

    Create Customer.java

---
+++ Customer.java
@@ -0,0 +1,15 @@
+class Customer {
+    String firstName;
+    String secondName;
+    int age;
+    int roomNumber; 
+    
+   
+    public void saveCustomerInfo() {
+        System.out.println("First name: " + firstName);
+        System.out.println("Second name: " + secondName);
+        System.out.println("Age: " + age);
+        System.out.println("Room number: " + roomNumber);
+    }
+ }
+ 
